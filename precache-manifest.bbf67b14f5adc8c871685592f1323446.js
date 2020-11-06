cb506daf77d5281cdd39bdb176f73dd6bd942aa7
Oleg <kolyushko.oleg@gmail.com>
2020-11-06 20:54:16 +0200

    Updates

---
+++ precache-manifest.bbf67b14f5adc8c871685592f1323446.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8c8fe2b3332a0fb780a3f56d318064",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "442a59f1c2a0f4620370",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d0c98eea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d0c98eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26fc62184b49312fa81c",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.d9ad9b39.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
